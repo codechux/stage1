450af0dc7b76674a3c883ffa7cffb96382b3ea9b
chux <chuxoguh@gmail.com>
2023-09-07 20:06:44 +0100

    adding minimal design and UTC milliseconds

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const personalData = {
-    name: "Oguh Chukwuka Erasmus",
-    pictureUrl: "/images/IMG_4930.JPG",
-    track: "Frontend Development",
+    slackDisplayName: "Chux",
+    slackDisplayPicture: "/images/IMG_4930.JPG",
+    track: "Frontend",
     githubUrl: "https://github.com/codechux",
+    githubTextContent: "Click here",
   };
 
-  document.getElementById("name").textContent = personalData.name;
-  document.getElementById("picture").src = personalData.pictureUrl;
+  document.getElementById("name").textContent = personalData.slackDisplayName;
+  document.getElementById("picture").src = personalData.slackDisplayPicture;
   document.getElementById("githubUrl").href = personalData.githubUrl;
-  document.getElementById("githubUrl").textContent = personalData.githubUrl;
+  document.getElementById("githubUrl").textContent =
+    personalData.githubTextContent;
   document.getElementById("track").textContent = personalData.track;
   document.getElementById("dayOfWeek").textContent =
     new Date().toLocaleDateString("en-US", { weekday: "long" });
-  document.getElementById("currentTime").textContent =
-    new Date().toLocaleTimeString();
+  document.getElementById("currentTime").textContent = new Date().getTime();
 });
